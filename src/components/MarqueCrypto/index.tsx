b4ed635d5035b4e9254f89e8277d62a5c2462e4e
shubham-kunwar <shubhamkunwar10feb@gmail.com>
2024-06-05 20:04:47 +0530

    Updates

--- src/components/MarqueCrypto/index.tsx
+++ src/components/MarqueCrypto/index.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectedCryptos } from "@/lib/slices/CryptoSlices/CryptoSlice";
 import { fetchSingleCryptoDispatcher } from "@/lib/slices/CryptoSlices/CryptoApiSlice";
 import { Box, Skeleton, Stack } from "@mui/material";
+import Marquee from "react-fast-marquee";
 
 const MarqueeCryptoNew = () => {
     const cryptoData = useSelector(selectedCryptos);
@@ -30,28 +31,29 @@ const MarqueeCryptoNew = () => {
     return (
         <section className="overflow-hidden mb-4 mt-2">
             <div className="flex flex-shrink-0 items-center justify-center rounded-2.5xl border border-jacarta-100 p-2">
+                <Marquee autoFill={true} pauseOnHover={true} gradient={false} > {/* Added 'items-center' to align items vertically */}
                 {cryptoData && cryptoData.map((crypto, index) => {
                     return (
-                        <div key={index} className="ml-8 animate-marquee flex items-center"> {/* Added 'items-center' to align items vertically */}
-
-                            <div className="flex items-center">
+                        <div key={index} className="ml-8  flex items-center"> {/* Added 'items-center' to align items vertically */}
+                            <div  className="flex items-center">
                                 {/* eslint-disable-next-line @next/next/no-img-element */}
                                 <img src={`/CryptoLogo/${crypto.cryptoData.cryptoSymbol}.png`} alt={crypto.cryptoData.cryptoSymbol} className="w-10 h-10 mb-2" />
                                 <Box className='ml-2'>
-                                    <div>{crypto.cryptoData.cryptoSymbol}</div>
+                   A                 <div>{crypto.cryptoData.cryptoSymbol}</div>
 
                                     {crypto.loading ? (
                                         <Stack spacing={1}>
                                             <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
                                         </Stack>
                                     ) : (
-                                        <div>${Number(crypto.cryptoData.price).toFixed(2)}</div>
+                                        <div>${Number(crypto?.cryptoData?.price).toFixed(2)}</div>
                                     )}
                                 </Box>
                             </div>
-                        </div>
+                            </div>
                     );
                 })}
+                </Marquee>
             </div>
         </section>
     );
