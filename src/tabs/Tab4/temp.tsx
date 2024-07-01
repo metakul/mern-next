9359cc1446e8a5d5763c7d88033e1cabde1541b5
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-07-01 13:26:15 +0530

    enable xr via button

--- src/tabs/Tab4/temp.tsx
+++ src/tabs/Tab4/temp.tsx
@@ -1,12 +1,24 @@
+import { Button, Container } from '@mui/material';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
 
 const ThreeScene = dynamic(() => import('@/components/Three.js/index'), { ssr: false });
 
 const Userpage: React.FC = () => {
+
+  const [xrEnabled,setXrEnabled]=useState(false)
+  const enableXr=()=>{
+    setXrEnabled(!xrEnabled)
+  }
+
   return (
-    <div>
-      <ThreeScene />
-    </div>
+    <Container>
+      <ThreeScene xrEnabled={xrEnabled} />
+
+      <Button onClick={enableXr}>
+        {xrEnabled ? "Disable VR" :"Enable VR"}
+      </Button>
+    </Container>
   );
 };
 
