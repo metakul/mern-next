945d0c47a5609bb723baf1f58f72e7d333b86c58
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-10-12 03:33:41 +0530

    update declaration

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,3 +1,6 @@
 declare module 'three/addons/webxr/VRButton.js';
 declare module 'three/examples/jsm/controls/OrbitControls.js';
 declare module 'three/addons/capabilities/WebGL.js';
+declare module 'react-helmet';
+declare module 'js-cookie';
+declare module 'three';
