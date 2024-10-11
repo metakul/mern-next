e1b99249d9bb562e1195ef4d01749f74d9d4e53b
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-10-12 03:14:09 +0530

    update config

---
+++ src/@customType/typing.d.ts
@@ -0,0 +1,9 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+      'coingecko-coin-price-marquee-widget': {
+        'coin-ids': string;
+        currency: string;
+        locale: string;
+      } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// src/global.d.ts
+
+declare module 'three/addons/webxr/VRButton.js';
+declare module 'three/addons/capabilities/WebGL.js';
+declare module 'three/examples/jsm/controls/OrbitControls.js';
+declare module 'three/examples/jsm/webxr/VRButton.js';
+declare module 'dat.gui';
+declare module 'three';
