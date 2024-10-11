e44871884fe92c6071dfb2922db1944cda8d7c3d
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-10-12 03:18:26 +0530

    Deplyment

--- src/@customType/three.d.ts
+++ src/@customType/three.d.ts
@@ -1,8 +1,1 @@
-// src/global.d.ts
-
-declare module 'three/addons/webxr/VRButton.js';
-declare module 'three/addons/capabilities/WebGL.js';
-declare module 'three/examples/jsm/controls/OrbitControls.js';
-declare module 'three/examples/jsm/webxr/VRButton.js';
-declare module 'dat.gui';
 declare module 'three';
