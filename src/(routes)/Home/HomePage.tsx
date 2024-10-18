0395d888cd2815401fd971b833778fdcbd7bd3b5
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-10-18 20:33:20 +0530

    Update meta tags

--- src/(routes)/Home/HomePage.tsx
+++ src/(routes)/Home/HomePage.tsx
@@ -5,9 +5,10 @@ import { HomePageProps } from '@/Datatypes/interfaces/interface';
 import { Container } from '@mui/material';
 import Tab1 from '@/tabs/Tab1';
 import { useSelector } from 'react-redux';
-import { isAuthenticated, selectUserType,  } from '@/lib/slices/authSlice';
+import { isAuthenticated, selectUserType, } from '@/lib/slices/authSlice';
 import { useNavigate } from 'react-router-dom';
 import { Pages, UserCategory } from '@/Datatypes/enums';
+import { Helmet } from 'react-helmet';
 
 
 
@@ -17,20 +18,41 @@ const HomePage: React.FC<HomePageProps> = () => {
   const isUserAuthenticated = useSelector(isAuthenticated);
   const userType = useSelector(selectUserType);
   const navigation = useNavigate()
- 
+
 
   useEffect(() => {
     if (isUserAuthenticated && userType == UserCategory.SUPER_ADMIN) {
-        navigation(Pages.DASHBOARD)
+      navigation(Pages.DASHBOARD)
     } else {
       console.error("User is not authenticated");
     }
   }, [isUserAuthenticated]);
-  
+
   return (
-     <Container className="container">
-        <Tab1/>
-     </Container>
+    <Container className="container">
+
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Metakul</title>
+        <link rel="canonical" href="https://metakul.live/" />
+        <meta name="description" content="Discover a gasless blockchain platform with NFTs, offering seamless transactions and passive crypto income through innovative staking and digital asset ownership." />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="metakul" />
+        <meta property="og:description" content="Discover a gasless blockchain platform with NFTs, offering seamless transactions and passive crypto income through innovative staking and digital asset ownership." />
+
+        <meta property="og:url" content="https://metakul.live/" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="metakul" />
+        <meta name="twitter:description" content="Discover a gasless blockchain platform with NFTs, offering seamless transactions and passive crypto income through innovative staking and digital asset ownership." />
+      </Helmet>
+
+      <Tab1 />
+    </Container>
   );
 };
 
