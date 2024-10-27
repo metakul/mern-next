f1966ce5dee995461307895a1b81c6ef44dab075
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-10-27 12:05:53 +0530

    Update client Id

--- src/scripts/signatureMint.tsx
+++ src/scripts/signatureMint.tsx
@@ -4,7 +4,6 @@ import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 const privateKey=import.meta.env.VITE_PUBLIC_PRIVATE_KEY_SIGNATURE as string
 const marketpalceAddress=import.meta.env.VITE_PUBLIC_MARKETPLACE_ADDRESS as string
 const secretKey=import.meta.env.VITE_PUBLIC_SECRET_KEY as string
-const clientId = import.meta.env.VITE_PUBLIC_THIRDWEB_CLIENT_ID as string;
 
 
 export default async function server({authorAddress,description, name:nftName, image}:any) {
@@ -13,7 +12,7 @@ export default async function server({authorAddress,description, name:nftName, i
         // De-structure the arguments we passed in out of the request body
 
         const sdk = ThirdwebSDK.fromPrivateKey(privateKey, 'polygon', {
-            clientId: clientId,
+            clientId: "14388476b18fbe83a550e76613c93c0f",
             secretKey: secretKey,
         });
         // Load the NFT Collection via it's contract address using the SDK
