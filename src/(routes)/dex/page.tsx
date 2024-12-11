05b5ee5cc0d50d8b065990c167f1021c8bddde50
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-12-11 20:58:00 +0530

    add link

--- src/(routes)/dex/page.tsx
+++ src/(routes)/dex/page.tsx
@@ -1,10 +1,19 @@
+import ContractInfo from '@/components/ContractInfo/ContractInfo'
 import CryptoTable from '@/tabs/Tab3'
 import React from 'react'
+
+const thirdwebDashboard = import.meta.env.VITE_THIRDWEB_DASHBOARD as string;
+const dexContractAddress = import.meta.env.VITE_PUBLIC_DEX_CONTRACT_ADDRESS as string;
+
 
 function DEXPage() {
   return (
     <div>
       <CryptoTable/>
+
+
+      <ContractInfo urlBase={`${thirdwebDashboard}/${dexContractAddress}`} buttonText="Metakul Liquiduty Dex Contract" />
+
     </div>
   )
 }
